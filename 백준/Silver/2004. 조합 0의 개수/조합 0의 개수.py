bd9c55ecbfb602327cc4e05dee1bce9118fd8ee0
Ji ho <97152750+jiho4399@users.noreply.github.com>
2023-11-02 21:27:42 +0900

    [Silver II] Title: 조합 0의 개수, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -0,0 +1,27 @@
+#n!의 5 개수 세는 함수
+def five_count(i):
+    answer = 0
+    while i != 0:
+        i = i // 5
+        answer += i
+    return answer
+
+#n!의 2 개수 세는 함수
+def two_count(i):
+    answer = 0
+    while i != 0:
+        i = i // 2
+        answer += i
+    return answer
+
+
+n, m = map(int, input().split())
+
+cnt_two = two_count(n)-two_count(m)-two_count(n-m)
+cnt_five = five_count(n)-five_count(m)-five_count(n-m)
+
+if m == 0:
+    print(0)
+    
+else:       
+    print(min(cnt_two, cnt_five))
