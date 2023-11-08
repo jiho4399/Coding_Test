d09eac87584043728834aba0177f40dbda116ff8
Ji ho <97152750+jiho4399@users.noreply.github.com>
2023-11-08 21:04:54 +0900

    [Bronze I] Title: 성 지키기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.py
@@ -0,0 +1,17 @@
+n, m = map(int,input().split())
+c = []
+
+for _ in range(n):
+    c.append(input())
+
+a, b = 0, 0
+
+for i in range(n):
+    if "X" not in c[i]:
+        a += 1
+
+for j in range(m):
+    if "X" not in [c[i][j] for i in range(n)]:
+        b += 1
+
+print(max(a ,b))
